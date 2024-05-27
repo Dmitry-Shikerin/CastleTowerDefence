f24dbbff8c634de4fac1653814d15b210bec8578
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-05-27 21:33:17 +0300

    add explosionBodyBloody

---
+++ Assets/Sources/BoundedContexts/ExplosionBodies/Presentation/Implementation/ExplosionBodyBloodyView.cs
@@ -0,0 +1,24 @@
+﻿using Sirenix.OdinInspector;
+using Sources.BoundedContexts.ExplosionBodies.Presentation.Interfaces;
+using Sources.Frameworks.Services.ObjectPools.Implementation.Destroyers;
+using Sources.Frameworks.Services.ObjectPools.Interfaces.Destroyers;
+using Sources.Presentations.Views;
+using UnityEngine;
+
+namespace Sources.BoundedContexts.ExplosionBodies.Presentation.Implementation
+{
+    public class ExplosionBodyBloodyView : View, IExplosionBodyBloodyView
+    {
+        [Required] [SerializeField] private ParticleSystem _particleSystem;
+        
+        private readonly IPODestroyerService _poDestroyerService = 
+            new PODestroyerService();
+        
+        private void OnEnable() =>
+            _particleSystem.Play();
+
+        private void OnParticleSystemStopped() =>
+            _poDestroyerService.Destroy(this);
+
+    }
+}
