642388cf2b1196b75082439281cc44e76cc00a2e
alisakorolevamira <105242972+alisakorolevamira@users.noreply.github.com>
2024-05-29 17:20:52 +0500

    add main menu hud

--- Assets/Plugins/Doozy/Runtime/UIManager/Ids/UIViewIdExtension.cs
+++ Assets/Plugins/Doozy/Runtime/UIManager/Ids/UIViewIdExtension.cs
@@ -17,6 +17,10 @@ namespace Doozy.Runtime.UIManager.Containers
         public static IEnumerable<UIView> GetViews(UIViewId.Gameplay id) => GetViews(nameof(UIViewId.Gameplay), id.ToString());
         public static void Show(UIViewId.Gameplay id, bool instant = false) => Show(nameof(UIViewId.Gameplay), id.ToString(), instant);
         public static void Hide(UIViewId.Gameplay id, bool instant = false) => Hide(nameof(UIViewId.Gameplay), id.ToString(), instant);
+
+        public static IEnumerable<UIView> GetViews(UIViewId.MainMenu id) => GetViews(nameof(UIViewId.MainMenu), id.ToString());
+        public static void Show(UIViewId.MainMenu id, bool instant = false) => Show(nameof(UIViewId.MainMenu), id.ToString(), instant);
+        public static void Hide(UIViewId.MainMenu id, bool instant = false) => Hide(nameof(UIViewId.MainMenu), id.ToString(), instant);
     }
 }
 
@@ -34,6 +38,14 @@ namespace Doozy.Runtime.UIManager
             Quit,
             Settings,
             Tutorial
+        }
+
+        public enum MainMenu
+        {
+            Background,
+            Hud,
+            Leaderboard,
+            Settings
         }    
     }
 }
