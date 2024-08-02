ffdc88e248e6a1b8e699f94e9a8e8d76e0070444
Dmitry-Shikerin <klubnr4ok@mail.ru>
2024-08-02 11:47:13 +0300

    fixed new game command

--- Assets/Sources/Frameworks/DoozyWrappers/SignalBuses/Infrastructure/ButtonCommands/Implementation/NewGameCommand.cs
+++ Assets/Sources/Frameworks/DoozyWrappers/SignalBuses/Infrastructure/ButtonCommands/Implementation/NewGameCommand.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using Doozy.Runtime.Signals;
+using JetBrains.Annotations;
 using Sources.BoundedContexts.Ids.Domain.Constant;
 using Sources.Frameworks.DoozyWrappers.SignalBuses.Domain.Domain;
 using Sources.Frameworks.DoozyWrappers.SignalBuses.Infrastructure.ButtonCommands.Interfaces;
 using Sources.Frameworks.GameServices.Loads.Services.Interfaces;
+using Sources.Frameworks.GameServices.Scenes.Services.Interfaces;
 using Sources.Frameworks.UiFramework.ServicesInterfaces.Forms;
 
 namespace Sources.Frameworks.DoozyWrappers.SignalBuses.Infrastructure.ButtonCommands.Implementation
@@ -11,11 +13,15 @@ namespace Sources.Frameworks.DoozyWrappers.SignalBuses.Infrastructure.ButtonComm
     public class NewGameCommand : IButtonCommand
     {
         private readonly ILoadService _loadService;
+        private readonly ISceneService _sceneService;
         private readonly IFormService _formService;
 
-        public NewGameCommand(ILoadService loadService)
+        public NewGameCommand(
+            ILoadService loadService,
+            ISceneService sceneService)
         {
             _loadService = loadService ?? throw new ArgumentNullException(nameof(loadService));
+            _sceneService = sceneService ?? throw new ArgumentNullException(nameof(sceneService));
         }
 
         public ButtonCommandId Id => ButtonCommandId.NewGame;
@@ -29,7 +35,7 @@ namespace Sources.Frameworks.DoozyWrappers.SignalBuses.Infrastructure.ButtonComm
                 return;
             }
 
-            Signal.Send(StreamId.MainMenu.NewGame, true);
+            _sceneService.ChangeSceneAsync(ModelId.Gameplay);
         }
     }
 }
