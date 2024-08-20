c8f06cffc3f0d6cf573ceddfad88095d469466c6
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-08-20 14:23:29 +0300

    add shaking for nuke ability

--- Assets/Sources/BoundedContexts/NukeAbilities/Infrastructure/Factories/Controllers/NukeAbilityPresenterFactory.cs
+++ Assets/Sources/BoundedContexts/NukeAbilities/Infrastructure/Factories/Controllers/NukeAbilityPresenterFactory.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using JetBrains.Annotations;
 using Sources.BoundedContexts.NukeAbilities.Controllers;
 using Sources.BoundedContexts.NukeAbilities.Domain.Models;
 using Sources.BoundedContexts.NukeAbilities.Presentation.Interfaces;
 using Sources.Frameworks.GameServices.Overlaps.Interfaces;
+using Sources.InfrastructureInterfaces.Services.Cameras;
 using Sources.InfrastructureInterfaces.Services.Repositories;
 
 namespace Sources.BoundedContexts.NukeAbilities.Infrastructure.Factories.Controllers
@@ -11,19 +13,23 @@ namespace Sources.BoundedContexts.NukeAbilities.Infrastructure.Factories.Control
     {
         private readonly IEntityRepository _entityRepository;
         private readonly IOverlapService _overlapService;
+        private readonly ICameraService _cameraService;
 
         public NukeAbilityPresenterFactory(
             IEntityRepository entityRepository,
-            IOverlapService overlapService)
+            IOverlapService overlapService,
+            ICameraService cameraService)
         {
             _entityRepository = entityRepository ?? 
                                 throw new ArgumentNullException(nameof(entityRepository));
             _overlapService = overlapService ?? throw new ArgumentNullException(nameof(overlapService));
+            _cameraService = cameraService ?? throw new ArgumentNullException(nameof(cameraService));
         }
 
         public NukeAbilityPresenter Create(INukeAbilityView view)
         {
-            return new NukeAbilityPresenter(_entityRepository, view, _overlapService);
+            return new NukeAbilityPresenter(
+                _entityRepository, view, _overlapService, _cameraService);
         }
     }
 }
