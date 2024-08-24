872970f32685aa262145576bdb999ddd690412cb
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-08-24 13:54:57 +0300

    refactoring characters

--- Assets/Sources/BoundedContexts/Characters/Controllers/States/CharacterInitializeState.cs
+++ Assets/Sources/BoundedContexts/Characters/Controllers/States/CharacterInitializeState.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using NodeCanvas.StateMachines;
 using ParadoxNotion.Design;
-using Sources.BoundedContexts.CharacterRanges.Domain;
-using Sources.BoundedContexts.CharacterRanges.Presentation.Implementation;
-using Sources.BoundedContexts.CharacterRanges.Presentation.Interfaces;
 using Sources.BoundedContexts.Characters.Domain;
+using Sources.BoundedContexts.Characters.Presentation.Implementation;
 using Sources.BoundedContexts.Characters.Presentation.Interfaces;
 using Sources.Frameworks.Utils.Reflections.Attributes;
 
-namespace Sources.BoundedContexts.CharacterRanges.Controllers.States
+namespace Sources.BoundedContexts.Characters.Controllers.States
 {
     [Category("Custom/Character")]
-    public class CharacterRangeInitializeState : FSMState
+    public class CharacterInitializeState : FSMState
     {
-        private ICharacterRangeView _view;
+        private ICharacterView _view;
         private ICharacterAnimation _animation;
         private Character _characterRange;
 
         [Construct]
-        private void Construct(Character characterRange, CharacterRangeView characterRangeView)
+        private void Construct(Character characterRange, CharacterView characterRangeView)
         {
             _characterRange = characterRange ?? throw new ArgumentNullException(nameof(characterRange));
             _view = characterRangeView ?? throw new ArgumentNullException(nameof(characterRangeView));
