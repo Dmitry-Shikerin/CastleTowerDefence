be8dc90d52ae92e6cda49849f0cb3f02893485ee
Dmitry_Shikerin <klubnr4ok@mail.ru>
2025-04-24 17:49:45 +0300

    add privacy policy data

---
+++ Assets/Sources/BoundedContexts/PrivacyPolicys/Presentation/PrivacyPolicyView.cs
@@ -0,0 +1,27 @@
+﻿using Doozy.Runtime.UIManager.Components;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Sources.BoundedContexts.PrivacyPolicys.Presentation
+{
+    public class PrivacyPolicyView : MonoBehaviour
+    {
+        [Required] [SerializeField] private UIButton _button;
+
+        private void OnEnable()
+        {
+            _button.onClickEvent.AddListener(OnClick);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClickEvent.RemoveListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            //TODO ссылка будет позже
+            //Application.OpenURL("");
+        }
+    }
+}
